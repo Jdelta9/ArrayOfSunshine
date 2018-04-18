6a2dd0e444fb42f2bc45effcda7410566bd56cab
Jdelta9 <35478858+Jdelta9@users.noreply.github.com>
2018-04-17 23:02:21 -0700

    Update app.js

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -10,12 +10,4 @@
   };
      firebase.initializeApp(config);
 
-       var more = $(this).attr("");
-       var queryURL = "https://arrayofsunshine-e9af0.firebaseio.com=" + more + "&api_key="AIzaSyCdMCfsevsjx8phekaS2VQZBLE5YbsZUdc";
-       $.ajax({
-            url: queryURL,
-        method: 'GET'
-    })
-</script>
-
-</html>
+     
