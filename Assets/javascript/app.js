cdb1591b1537cfb39f8c81aca93881fd226a8f5f
Jessica Cervantes <J1123C@gmail.com>
2018-04-20 23:40:13 -0700

    Add flashcards

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,13 +1,66 @@
+$(function () {
+  /* onread execute js */
 
-        // Initialize Firebase
-    var config = {   
-    apiKey: "AIzaSyCdMCfsevsjx8phekaS2VQZBLE5YbsZUdc",
-    authDomain: "arrayofsunshine-e9af0.firebaseapp.com",
-    databaseURL: "https://arrayofsunshine-e9af0.firebaseio.com",
-    projectId: "arrayofsunshine-e9af0",
-    storageBucket: "arrayofsunshine-e9af0.appspot.com",
-    messagingSenderId: "365773571173"
-  };
-     firebase.initializeApp(config);
+  /* card data js */
+  $.ajax({
+    url: 'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple',
+    method: 'GET',
+  }).then(function (response) {
+    processData(response);
+  });
 
-     
+
+  function processData(response){
+    console.log(response);
+    var stack = $('#stack');
+    var results = response.results;
+    var card_top = 0;
+    var card_right = 0;
+    var card_z_index = results.length;
+
+    for (var i = 0; i < results.length; i++){
+      var card = createNewCard(i);
+      var front_card = card.children('.front'); // reference to inner div "front"
+      var back_card = card.children('.back'); // reference to inner div "back"
+
+      front_card.html(results[i].question); // add question data
+      back_card.html(results[i].correct_answer); // add answer data
+
+      // add style
+      card.attr('style', 'top: '+card_top+'px; right: '+card_right+'px; z-index: '+card_z_index+';')
+      
+      // increment card positions and z-index
+      card_top += 3;
+      card_right += 2;
+      card_z_index--;
+
+      stack.append(card);
+    }
+
+  }
+
+  /* create a blank card object */
+  function createNewCard(i){
+    var card = $('<div>');
+    var front = $('<div>');
+    var back = $('<div>');
+
+    card.addClass('card');
+    front.addClass('front');
+    back.addClass('back');
+
+    // add listener to flip card
+    card.click(function () {
+      $(this).addClass('flipped');
+      $(this).find('.front').addClass('showingBack');
+      $(this).find('.front').css("z-index", 0);
+      $(this).css("z-index", i);
+    });
+
+    card.append(front).append(back);
+
+    return card;
+  }
+
+
+});
